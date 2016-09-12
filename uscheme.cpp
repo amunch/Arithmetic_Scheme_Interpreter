0f3c4db7db781170a6050959900b1923e0b6f13b
amunch <amunch@nd.edu>
2016-09-11 20:35:28 -0400

    Working on uscheme

--- uscheme.cpp
+++ uscheme.cpp
@@ -1,4 +1,5 @@
 // uscheme.cpp
+//By Mark Pruitt and Andrew Munch
 
 #include <iostream>
 #include <sstream>
@@ -27,6 +28,35 @@ struct Node {
 };
 
 ostream &operator<<(ostream &os, const Node &n) {
+    stack<Node> nums;
+    stack<bool> side;
+    nums.push(n);
+
+
+    while(!nums.empty()) {
+        auto temp = nums.top();
+        nums.pop();
+        cout << "(Node: value=";
+        cout << temp.value;      
+        if(!side.empty()) {
+            if(side.top()) {
+                cout << "left=";
+            }
+            else {
+                cout << "right=";
+            }
+            side.pop();
+        }
+    
+        if(temp.left) {
+            nums.push(*temp.left);
+            side.push(1);
+        }
+        if(temp.right) {
+            nums.push(*temp.right);
+            side.push(0);
+        }
+    }
     return os;
 }
 
@@ -34,20 +64,81 @@ ostream &operator<<(ostream &os, const Node &n) {
 
 string parse_token(istream &s) {
     string token;
+    char next;
+    char temp;
+    next = s.peek();
+    if(isspace(next)) {
+        temp = s.get();
+    }
+
+    next = s.peek();
+    if((next == '(') || (next == ')') || (next == '+') || (next == '-') || (next == '*') || (next == '/')) {
+        token = s.get();
+    }
+    else {
+        char next_num = s.peek();
+        while(isdigit(next_num)) {
+            token = token + s.get();
+        }
+    } 
+
     return token;
 }
 
 Node *parse_expression(istream &s) {
+    string token = parse_token(s);
+
+    if((token == "") || (token == ")") {
+        return nullptr;
+    }
+    elseif(token == "(") {
+        token = parse_token(s);
+        auto left = parse_expression(s);
+        if(left) {
+            auto right = parse_expression(s);
+        }
+        if(right) {
+            parse_token(s);
+        }
+    }
+
     return new Node(token, left, right);
 }
 
 // Interpreter -----------------------------------------------------------------
 
 void evaluate_r(const Node *n, stack<int> &s) {
+    if(n->left()) {
+        evaluate_r(n->left, s);
+        evaluate_r(n->right, s);
+    }
+    if(isdigit(n->value[0])) {
+        s.push(atoi(n->value.cstr()));
+    }
+    else {
+        first = atoi(s.top().c_str());
+        s.pop();
+        second = atoi(s.top().c_str());
+        s.pop();
+        if(n->value == "+") {
+            s.push(first + second);
+        }
+        if(n->value == "-") {
+            s.push(first - second);
+        }
+        if(n->value == "/") {
+            s.push(first / second);
+        }
+        if(n->value == "*") {
+            s.push(first * second);
+        }
+    }
 }
 
 int evaluate(const Node *n) {
-    return 0;
+    stack<int> s;
+    evaluate_r(n, s);
+    return s.top();
 }
 
 // Main execution --------------------------------------------------------------
@@ -92,4 +183,3 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-// vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
